451483ef27bb0e44cac6d7974a796afa0ef818ce
seto <seto@catchup.co.jp>
2021-04-10 22:11:54 +0900

    add

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # CaptureScreenshot
+
+指定したURLのスクリーンショットを取得します。
+
+- npm install
+- url.txtにスクリーンショットを取得したいURLを一行ずつ記載
+- node init.js
---
+++ init.js
@@ -0,0 +1,27 @@
+const { chromium, devices } = require('playwright');
+const fs = require('fs').promises;
+
+let now = new Date();
+let nowText =  now.getFullYear()
+  + ('0' + (now.getMonth() + 1)).slice(-2)
+  + ('0' + now.getDate()).slice(-2)
+  + ('0' + now.getHours()).slice(-2)
+  + ('0' + now.getMinutes()).slice(-2)
+  + ('0' + now.getSeconds()).slice(-2);
+
+(async () => {
+  const text = await fs.readFile('url.txt', 'utf-8');
+  const urls = text.split('\n');
+  browser = await chromium.launch();
+  context = await browser.newContext();
+  const page = await context.newPage();
+  for (let url of urls) {
+    if (!url) continue;
+    let screenshotPath = 'screenshot/' + nowText + '/' + encodeURIComponent(url) + '.png';
+    console.log(url);
+    await page.goto(url);
+    await page.screenshot({ path: screenshotPath, fullPage: true });
+  }
+  await context.close();
+  await browser.close();
+})();
